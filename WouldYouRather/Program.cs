97428d80c4b6a48b544075c73dc68f7e756ecdfb
Rblye22 <rpblye@email.neit.edu>
2026-08-24 11:46:14 -0400

    End of day
    Database is set up and need to connect code to the database still.

--- WouldYouRather/Program.cs
+++ WouldYouRather/Program.cs
@@ -22,4 +22,84 @@ app.UseAuthorization();
 
 app.MapRazorPages();
 
+
+QuestionsDatabase db = new QuestionsDatabase();
+
+app.MapGet("/api/questions/random", () =>
+{
+Question q = db.GetRandomQuestion();
+return Results.Ok(q);
+});
+
+app.MapPost("/api/vote", (VoteRequest vote) =>
+{
+db.AddVote(vote.QuestionId, vote.Choice);
+return Results.Ok(new { message = "Vote recorded" });
+});
+
+app.MapGet("/api/results", () =>
+{
+return Results.Ok(db.GetAllQuestions());
+});
+
 app.Run();
+public class VoteRequest
+{
+    public int QuestionId { get; set; }
+    public string Choice { get; set; }
+}
+public class Question
+{
+    public int Id { get; set; }
+    public string OptionA { get; set; }
+    public string OptionB { get; set; }
+    public int VotesA { get; set; }
+    public int VotesB { get; set; }
+    public double PercentA { get; set; }
+    public double PercentB { get; set; }
+
+    public Question(int id, string optionA, string optionB)
+    {
+        Id = id;
+        OptionA = optionA;
+        OptionB = optionB;
+    }
+}
+public class QuestionsDatabase
+{
+    private List<Question> questions = new List<Question>
+    {
+        new Question(1, "Have the ability to fly", "Have the ability to become invisible"),
+        new Question(2, "Always be 10 minutes late", "Always be 20 minutes early"),
+        new Question(3, "Give up pizza forever", "Give up burgers forever"),
+        new Question(4, "Fight one horse-sized duck", "Fight 100 duck-sized horses"),
+        new Question(5, "Have unlimited pancakes", "Have unlimited waffles")
+    };
+    public List<Question> GetAllQuestions()
+    {
+        return questions;
+    }
+    public Question GetRandomQuestion()
+    {
+        Random rnd = new Random();
+        return questions[rnd.Next(questions.Count)];
+    }
+    public void AddVote(int questionId, string choice)
+    {
+        foreach (Question q in questions)
+        {
+            if (q.Id == questionId)
+            {
+                if (choice == "A") q.VotesA++;
+                if (choice == "B") q.VotesB++;
+
+                int total = q.VotesA + q.VotesB;
+                if (total > 0)
+                {
+                    q.PercentA = Math.Round((double)q.VotesA / total * 100, 1);
+                    q.PercentB = Math.Round((double)q.VotesB / total * 100, 1);
+                }
+            }
+        }
+    }
+}
---
+++ WouldYouRather/wwwroot/js/Results.js
@@ -0,0 +1,34 @@
+﻿async function loadResults() {
+    var response = await fetch('/api/results');
+    var questions = await response.json();
+
+    var container = document.getElementById('resultsContainer');
+    container.innerHTML = '';
+
+    for (var i = 0; i < questions.length; i++) {
+        var q = questions[i];
+
+        var totalVotes = q.votesA + q.votesB;
+        if (totalVotes == 0) {
+            continue;
+        }
+
+        var box = document.createElement('div');
+        box.className = 'Questions';
+        var html = '<div class="options">';
+        html += '<div class="option" style="background-color: teal;">';
+        html += '<p>' + q.optionA + '</p>';
+        html += '<p>' + q.percentA + '%</p>';
+        html += '</div>';
+        html += '<div class="or-label">VS</div>';
+        html += '<div class="option" style="background-color: purple;">';
+        html += '<p>' + q.optionB + '</p>';
+        html += '<p>' + q.percentB + '%</p>';
+        html += '</div>';
+        html += '</div>';
+
+        box.innerHTML = html;
+        container.appendChild(box);
+    }
+}
+loadResults();
